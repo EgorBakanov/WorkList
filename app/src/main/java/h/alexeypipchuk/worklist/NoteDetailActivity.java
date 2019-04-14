b5111997e5e1808901ed02f1c3f4d2d00d024793
Баканов Егор <xxxprotosxxx@mail.ru>
2019-04-14 13:15:19 +0700

    Migrate to androidx

--- app/src/main/java/h/alexeypipchuk/worklist/NoteDetailActivity.java
+++ app/src/main/java/h/alexeypipchuk/worklist/NoteDetailActivity.java
@@ -1,6 +1,6 @@
 package h.alexeypipchuk.worklist;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
